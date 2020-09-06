735b14290aa78efdd7fbff3d250071cb105248db
Per Thykjær Jensen <petj@eaaa.dk>
2020-09-06 08:35:12 +0200

    front-page.php etc

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-/*This file is part of MultimusenTwentyTwentyChild, twentytwenty child theme.
+/*This file is part of Pers2020Child, twentytwenty child theme.
 
 All functions of this file will be loaded before of parent theme functions.
 Learn more at https://codex.wordpress.org/Child_Themes.
@@ -9,7 +9,7 @@ Note: this function loads the parent stylesheet before, then child theme stylesh
 */
 
 if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
-	function MultimusenTwentyTwentyChild_enqueue_child_styles() {
+	function Pers2020Child_enqueue_child_styles() {
 	    // loading parent style
 	    wp_register_style(
 	      'parente2-style',
@@ -25,6 +25,22 @@ if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
 	    wp_enqueue_style( 'childe2-style');
 	 }
 }
-add_action( 'wp_enqueue_scripts', 'MultimusenTwentyTwentyChild_enqueue_child_styles' );
+add_action( 'wp_enqueue_scripts', 'Pers2020Child_enqueue_child_styles' );
 
 /*Write here your own functions */
+
+// PETJ GUTENBERG HACKS
+
+// new post: now with an img as first block
+function petj_register_template() {
+    $post_type_object = get_post_type_object( 'post' );
+    $post_type_object->template = array(
+        array( 'core/image' ),
+    );
+}
+add_action( 'init', 'petj_register_template' );
+
+
+// experiment
+
+
---
+++ page-pertest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for the page with the slug pertest
+ *
+ * @package WordPress: twentytwenty child
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_header();
+?>
+
+<!-- main id="site-content" role="main" -->
+
+	
+
+        <h1> TEST:  </h1>
+        <h2>page-pertest.php</h2>
+        
+        <p>In Settings / Reading : set a static front page.</p>
+
+	    <p>A static front page, featuring:</p>
+	    <ul>
+	        <li>No loop</li>
+	        <li>Pure HTML / CSS</li>
+	        <li>Only header.php and footer.php loaded</li>
+	        <li>Styles from the parent and child theme</li>
+	    </ul>
+	    
+	    <p>Based on: 404.php</p>
+	    
+	    <img src="http://multimusen.dk/wpsandbox/wp-content/uploads/2008/06/dsc09114.jpg" alt="Image sample" style="width:200px;height:auto;transform:rotate(12deg);">
+	    
+        <!-- start slipsum code -->
+
+<h2>Dummy content from <a href="https://slipsum.com/"> slipsum.com </a></h2>
+
+<h4>I can do that</h4>
+Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
+
+<h4>No man, I don't eat pork</h4>
+Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
+
+<h4>Is she dead, yes or no?</h4>
+Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+
+<!-- end slipsum code -->
+	
+
+<!-- /main --><!-- #site-content -->
+
+<?php 
+
+// don't display the widgets:
+// get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
