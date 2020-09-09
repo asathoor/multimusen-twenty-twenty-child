0ededcb72dd12fd617c91457a283f125b2cd9a05
Per Thykjær Jensen <petj@eaaa.dk>
2020-09-09 14:03:21 +0200

    frontpage again
    fresh folder now

--- front-page.php
+++ front-page.php
@@ -12,9 +12,13 @@ get_header();
 
 <!-- main id="site-content" role="main" -->
 
-<main>
+<main id="frontPage">
     <!-- YouTube -->
-    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/1o645n7OeeA?autoplay=1&muted=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe> 
+    <iframe width="100%" height="400px" src="https://www.youtube.com/embed/1o645n7OeeA?autoplay=1&muted=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+    <aside id="fileInfo">
+    	File: front-page.php<br>
+	Purpose: If you choose a static front page WordPress will use the file <em>front-page.php</em>.
+    </aside>
 </main>	    
 
 
